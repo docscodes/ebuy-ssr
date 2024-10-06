6861fda12c4928f5238cd615cdf00583f49ea422
docscodes <docscodes@gmail.com>
2024-10-06 11:22:44 -0400

    Creating the checkout micro app

---
+++ apps/checkout/next.config.js
@@ -0,0 +1,31 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    checkout: `checkout@http://localhost:3002/_next/static/${location}/remoteEntry.js`,  
+  };
+};
+
+module.exports = {
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "checkout",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./Module": "./pages/index.tsx",
+        },
+        remotes: remotes(options.isServer),
+        shared: {},
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+      })
+    );
+
+    return config;
+  },
+};
