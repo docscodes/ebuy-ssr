e47428b4f52eba82077f004b0e1c6e9fabcecf7e
docscodes <docscodes@gmail.com>
2024-10-09 10:27:52 -0400

    fix remotes

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -10,7 +10,7 @@ const remotes = (isServer) => {
   const CHECKOUT_URL_LOCAL = "http://localhost:3002";
 
   const CATALOG_URL_PROD = "http://4.155.182.196";
-  const CHECKOUT_URL_PROD = "http://<your-k8s-ip-address>";
+  const CHECKOUT_URL_PROD = "http://4.155.182.196";
 
   const CATALOG_REMOTE_HOST =
     ENV === "PROD" ? CATALOG_URL_PROD : CATALOG_URL_LOCAL;
